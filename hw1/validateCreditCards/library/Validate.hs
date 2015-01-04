d29f9efef97103fdfcaae60dd388975283fae59b
Mike Gehard <mike.gehard@gmail.com>
2015-01-04 16:43:49 -0700

    Ability to break numbers down into a list of digits
    This also taught me how to use QuickCheck and HSpec.

--- hw1/validateCreditCards/library/Validate.hs
+++ hw1/validateCreditCards/library/Validate.hs
@@ -1,3 +1,13 @@
-module Validate (validate) where
+module Validate where
+
+import Data.Char (digitToInt)
 
 validate s = "Validate! " ++ s
+
+toDigits :: Integer -> [Integer]
+toDigits n
+    | n > 0 = map (toInteger.digitToInt) (show n)
+    | otherwise = []
+
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev n = reverse(toDigits n)
--- hw1/validateCreditCards/test-suite/ValidateSpec.hs
+++ hw1/validateCreditCards/test-suite/ValidateSpec.hs
@@ -1,10 +1,31 @@
 module ValidateSpec (spec) where
 
-import Validate (validate)
+import Validate
 import Test.Hspec
+import Test.QuickCheck
+
 
 spec :: Spec
 spec = do
     describe "validate" $ do
         it "returns something" $ do
             validate "me" `shouldBe` "Validate! me"
+
+    describe "toDigits" $ do
+--        it "returns empty list for 0" $ do
+--            toDigits 0 `shouldBe` []
+--
+--        it "returns empty list for negative numbers" $ do
+--            toDigits (-1) `shouldBe` []
+--
+        -- the above two can be handled using QuickCheck properties
+        it "returns empty list for numbers less than or equal to 0" $ do
+            property $ \n -> n <= 0 ==>
+                toDigits n == []
+
+        it "breaks the number up into digits" $ do
+            toDigits 12345 `shouldBe` [1,2,3,4,5]
+
+    describe "toDigitsRev" $ do
+        it "returns the list reversed" $ do
+            toDigitsRev 12345 `shouldBe` [5,4,3,2,1]
